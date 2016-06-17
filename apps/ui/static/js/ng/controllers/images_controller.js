38a03577f5ebf471bfe7429b361ba9801ab35de3
Hardik Patel <hpatel@vavni.com>
2016-06-17 18:57:17 +0530

    HTML and Controller modified according to updated listing API.

--- apps/ui/static/js/ng/controllers/images_controller.js
+++ apps/ui/static/js/ng/controllers/images_controller.js
@@ -9,9 +9,8 @@ imagesPage.controller("imagesPageController",
         // Load photos list with thumb URLS
         $scope.url = '/api/catalog/images/';
         var url = $scope.url;
-        $http.get(url).success(function (data) {
-            console.log(data[0].image.thumbnail)
-            $scope.photos = data;
+        $http.get(url).success(function (data) {            
+            $scope.photos = data.results;
         });
         // Function to get images.
         $scope.getImages = function(){
